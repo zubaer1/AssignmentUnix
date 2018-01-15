83f8c6630990d194035676403cabf34d4a643483
dhaka3049 <ishugrd@gmail.com>
2018-01-15 13:03:54 -0500

    Assignment Ready for submit

---
+++ Makefile
@@ -0,0 +1,19 @@
+all: test1.txt README.md
+
+text1.txt:
+    touch text1.txt
+    echo “This is Zubaer “>>test1.txt
+    echo “This is assignment for UNIX”>>test1.txt
+  
+README.md:
+    touch README.md
+    echo “#This is README.md file for Peer-graded Assignment.”>README.md
+    echo “##Title of Project: Project for Unix## ”>>README.md
+    echo “**Date and Time:   **” >>README.md
+    date %c >>README.md
+    wc -l guessinggame.sh >>README.md
+  
+clean:
+    rm text1.txt
+
+
---
+++ guessinggame.sh
@@ -0,0 +1,35 @@
+
+function file_number(){
+file=6
+echo "Please Type, how many files are in the current directory: "
+read number
+echo "You typed: $number"
+
+while [ $number -eq $file ]
+    do
+echo “Congratulation, you file number is $file”
+break
+    done 
+
+if [[ $file -gt $1 ]]  &&  [[ 9 -gt $1 ]]
+then
+ 	echo "Please try again, type less than 9"
+elif [[ $file -gt $1 ]]  &&  [[ 9 -lt  $1 ]]
+then
+	echo " Please try again with less number 9"
+
+elif [[ $file -lt $1 ]]  &&  [[ 1 -lt  $1 ]]
+then
+echo " Please try again with higher than 1"
+else
+echo "Please type greater than 1"
+fi
+
+echo "End program"
+##quit
+exit
+}
+
+
+file_number
+
