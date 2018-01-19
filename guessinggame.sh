4d11b5c68e4e66420c5b4b8dda8be662a6bd2495
Zubaer I Ahmed <ishugrd@gmail.com>
2018-01-19 12:01:21 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,27 +1,35 @@
-function file_number(){
-file=6
-echo "Please Type, how many files are in the current directory: "
-read number
-echo "You typed: $number"
-while [ $number -eq $file ]
-    do
-echo “Congratulation, you file number is $file”
-break
-    done 
-if [[ $file -gt $1 ]]  &&  [[ 9 -gt $1 ]]
-then
- 	echo "Please try again, type less than 9"
-elif [[ $file -gt $1 ]]  &&  [[ 9 -lt  $1 ]]
-then
-	echo " Please try again with less number 9"
-elif [[ $file -lt $1 ]]  &&  [[ 1 -lt  $1 ]]
-then
-echo " Please try again with higher than 1"
-else
-echo "Please type greater than 1"
-fi
-echo "End program"
-exit
-}
-file_number
+#!/bin/bash
+# File: guessinggame.sh
 
+numfiles=$(( ls -1 | wc -l ))
+
+echo "Guess how many files are in the current folder!"
+
+guess="-1"
+
+function countFilesInDir ()
+{
+    ls -1 | wc -l
+}
+
+
+while [[ $(countFilesInDir) != $guess ]]
+do
+    read guess
+    if ! [[ "$guess" =~ ^[0-9]+$ ]]
+    then
+        echo "Invalid input! Please try again!"
+
+    elif (( guess > $(countFilesInDir) ))
+    then
+        echo "The number is too high. please try again!:"
+
+    elif (( guess < $(countFilesInDir) ))
+    then
+        echo "The number is too low. please try again!:"
+
+    else
+        echo "Correct"
+        break
+    fi
+done
