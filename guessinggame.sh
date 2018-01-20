1737c71723b78ed0bbaf8c050541f6381678b161
Zubaer I Ahmed <ishugrd@gmail.com>
2018-01-20 11:33:27 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,23 +3,23 @@ numfiles=$(( ls -1 | wc -l ))
 echo "Guess how many files are in this folder!"
 
 number="-1"
-function countFilesInDir ()
+function countFile ()
 {
     ls -1 | wc -l
 }
 
-while [[ $(countFilesInDir) != $number ]]
+while [[ $(countFile) != $number ]]
 do
     read number
     if ! [[ "$number" =~ ^[0-9]+$ ]]
     then
         echo "Invalid input. Please try again"
 
-    elif (( number > $(countFilesInDir) ))
+    elif (( number > $(countFile) ))
     then
         echo "The number is too high. Please try again"
 
-    elif (( number < $(countFilesInDir) ))
+    elif (( number < $(countFile) ))
     then
         echo "The number is too low. Please try again"
 
